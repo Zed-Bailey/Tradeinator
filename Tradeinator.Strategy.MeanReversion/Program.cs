95d4d6e261fdcd88b1cf5c869c50b5e983a86982
Zoran <emailzed03@gmail.com>
2023-12-14 15:44:25 +1100

    logging

--- Tradeinator.Strategy.MeanReversion/Program.cs
+++ Tradeinator.Strategy.MeanReversion/Program.cs
@@ -1,4 +1,6 @@
-﻿using Tradeinator.Shared;
+﻿using Serilog;
+using Tradeinator.Shared;
+using Tradeinator.Shared.Models;
 
 // implementation of the mean reversion strategy from
 // https://github.com/alpacahq/alpaca-trade-api-csharp/blob/develop/UsageExamples/MeanReversionPaperOnly.cs
@@ -7,10 +9,27 @@ const string host = "localhost";
 const string exchangeName = "test_exchange";
 
 using var exchange = new ReceiverExchange(host, exchangeName, "bar.ETH/USD");
+var tokenSource = new CancellationTokenSource();
+
+
+ // initialise serilog logger, writing to console and file
+await using var logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("mean_reversion.log")
+    .CreateLogger();
+
+
+// handle ctrl-c
+Console.CancelKeyPress += (sender, eventArgs) =>
+{
+    logger.Information("Cancellation requested");
+    tokenSource.Cancel();
+};
 
 exchange.ConsumerOnReceive += (sender, eventArgs) =>
 {
-
+    var bar = eventArgs.SerializeToModel<Bar>();
+    Console.WriteLine(eventArgs.BodyAsString());
 };
 
 
