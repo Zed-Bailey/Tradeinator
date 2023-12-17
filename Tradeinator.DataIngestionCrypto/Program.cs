17e416a839f5bfbf1488aa13b3f38f708ab8fd96
Zoran <emailzed03@gmail.com>
2023-12-17 17:11:26 +1100

    updated crypto data ingestion project

--- Tradeinator.DataIngestionCrypto/Program.cs
+++ Tradeinator.DataIngestionCrypto/Program.cs
@@ -1,3 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Alpaca.Markets;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Tradeinator.DataIngestion.Shared;
+using Tradeinator.Shared;
 
-Console.WriteLine("Hello, World!");
+// load the dotenv file into the environment
+DotEnv.LoadEnvFiles(Path.Combine(Directory.GetCurrentDirectory(), ".env"));
+
+// load the config
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", true)
+    .AddEnvironmentVariables()
+    .Build();
+
+var host = config["Rabbit:Host"];
+var exchangeName = config["Rabbit:Exchange"];
+
+if (host is null || exchangeName is null)
+{
+    throw new ArgumentNullException(host ?? exchangeName);
+}
+
+using var exchange = new PublisherExchange(host, exchangeName);
+
+// initialise serilog logger, writing to console and file
+await using var logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("data_ingestion.log")
+    .CreateLogger();
+
+
+
+//
+
+using var client = Environments.Paper.GetAlpacaTradingClient(new SecretKey(config["ALPACA_KEY"],config["ALPACA_SECRET"]));
+
+var clock = await client.GetClockAsync();
+
+Console.WriteLine(
+    "Timestamp: {0}, NextOpen: {1}, NextClose: {2}",
+    clock.TimestampUtc, clock.NextOpenUtc, clock.NextCloseUtc);
+
+
+
+using var dataStreaming = Environments.Paper.GetAlpacaDataStreamingClient(new SecretKey(config["ALPACA_KEY"],config["ALPACA_SECRET"]));
+dataStreaming.RegisterLoggers(logger);
+
+var authStatus = await dataStreaming.ConnectAndAuthenticateAsync();
+if (authStatus == AuthStatus.Unauthorized || authStatus == AuthStatus.TooManyConnections)
+{
+    logger.Error("Failed to create a data streaming subscription, authStatus: {Status}", authStatus);
+    return;
+}
+    
+
+
+// watches the symbols file and will subscribe and unsubscribe from data streams when the file changes
+await using var subscriptionManager = new StockSubscriptionManager(logger, exchange, dataStreaming, Directory.GetCurrentDirectory(), "symbols.txt");
+
+logger.Information("Started watching symbols file: {Path}", subscriptionManager.SymbolsFile);
+
+var tokenSource = new CancellationTokenSource();
+
+Console.CancelKeyPress += (sender, eventArgs) =>
+{
+    logger.Information("Cancellation Requested");
+    subscriptionManager.UnsubscribeFromAll().Wait();
+    tokenSource.Cancel();
+};
+
+await Task.Delay(-1, tokenSource.Token);
+
+
--- Tradeinator.DataIngestionStocks/Program.cs
+++ Tradeinator.DataIngestionStocks/Program.cs
@@ -43,10 +43,8 @@ Console.WriteLine(
     clock.TimestampUtc, clock.NextOpenUtc, clock.NextCloseUtc);
 
 
-// using var dataStreaming = Environments.Paper.GetAlpacaDataStreamingClient(new SecretKey(config["ALPACA_KEY"],config["ALPACA_SECRET"]));
-// dataStreaming.RegisterLoggers(logger);
 
-using var dataStreaming = Environments.Paper.GetAlpacaCryptoStreamingClient(new SecretKey(config["ALPACA_KEY"],config["ALPACA_SECRET"]));
+using var dataStreaming = Environments.Paper.GetAlpacaDataStreamingClient(new SecretKey(config["ALPACA_KEY"],config["ALPACA_SECRET"]));
 dataStreaming.RegisterLoggers(logger);
 
 var authStatus = await dataStreaming.ConnectAndAuthenticateAsync();
@@ -59,7 +57,7 @@ if (authStatus == AuthStatus.Unauthorized || authStatus == AuthStatus.TooManyCon
 
 
 // watches the symbols file and will subscribe and unsubscribe from data streams when the file changes
-await using var subscriptionManager = new SubscriptionManager(logger, exchange, dataStreaming, Directory.GetCurrentDirectory(), "symbols.txt");
+await using var subscriptionManager = new StockSubscriptionManager(logger, exchange, dataStreaming, Directory.GetCurrentDirectory(), "symbols.txt");
 
 logger.Information("Started watching symbols file: {Path}", subscriptionManager.SymbolsFile);
 
