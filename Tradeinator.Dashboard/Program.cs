f4b3a8eee5df2f00beedb872eadc2660da8541e8
Zoran <emailzed03@gmail.com>
2024-01-10 12:27:57 +1100

    add fluent ui

--- Tradeinator.Dashboard/Program.cs
+++ Tradeinator.Dashboard/Program.cs
@@ -1,13 +1,30 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.FluentUI.AspNetCore.Components;
 using Tradeinator.Dashboard.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+var host = configuration["Rabbit:Host"];
+var exchangeName = configuration["Rabbit:Exchange"];
+if(host is null || exchangeName is null)
+{
+    throw new ArgumentNullException("RabbitMQ host or exchange name was null in config file");
+}
+
+var exchange = new EventService(host, exchangeName);
+await exchange.StartConsuming();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<EventService>(exchange);
+
+// register default http client, required for fluent ui
+builder.Services.AddHttpClient();
+builder.Services.AddFluentUIComponents();
 
 var app = builder.Build();
 
