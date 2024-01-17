76e4d4254c9dfea76982fcf7d5d9e0f802a21b16
Zoran <emailzed03@gmail.com>
2024-01-17 13:16:44 +1100

    added function to invoke event handler as derived classes cant access it directly

--- Tradeinator.Strategy.Shared/StrategyBase.cs
+++ Tradeinator.Strategy.Shared/StrategyBase.cs
@@ -8,6 +8,16 @@ public abstract class StrategyBase : IDisposable
 {
 
     public event EventHandler<SystemMessageEventArgs>? SendMessageNotification;
+
+    /// <summary>
+    /// Used to invoke the event handler as a derived class cant directly access it
+    /// see -> https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/events/how-to-raise-base-class-events-in-derived-classes
+    /// </summary>
+    /// <param name="e">the system message to send</param>
+    protected virtual void OnSendMessage(SystemMessageEventArgs e)
+    {
+        SendMessageNotification?.Invoke(this, e);
+    }
     
     public virtual Task Init()
     {
