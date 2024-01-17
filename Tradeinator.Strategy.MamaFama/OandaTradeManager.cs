76e8cf5071b818fecef1ee1bf8e42c45596863dc
Zoran <emailzed03@gmail.com>
2024-01-17 13:16:13 +1100

    functionality to open positions

--- Tradeinator.Strategy.MamaFama/OandaTradeManager.cs
+++ Tradeinator.Strategy.MamaFama/OandaTradeManager.cs
@@ -1,12 +1,13 @@
 using GeriRemenyi.Oanda.V20.Client.Model;
 using GeriRemenyi.Oanda.V20.Sdk;
 using GeriRemenyi.Oanda.V20.Sdk.Common.Types;
+using GeriRemenyi.Oanda.V20.Sdk.Trade;
 
 namespace Tradeinator.Strategy.MamaFama;
 
 public class OandaTradeManager
 {
-    private string _apiToken;
+    private readonly string _apiToken;
     private IOandaApiConnection _oandaApiConnection;
     
     
@@ -38,8 +39,50 @@ public class OandaTradeManager
         return res;
     }
 
-    public async Task OpenLongPosition(string account)
+    public async Task<CreateOrderResponse> OpenLongPosition(string account, double units, double stopLossPrice)
     {
-        _oandaApiConnection.OrderApi.CreateOrder()
+        var stoploss = new StopLossDetails(
+                price: stopLossPrice
+        );
+
+        var order = new MarketOrder(
+            InstrumentName.AUD_CHF,
+            units,
+            stopLossOnFill: stoploss
+        );
+
+        var orderRequest = new CreateOrderRequest(new
+        {
+            Instrument = order.Instrument,
+            Units = order.Units,
+            StopLossOnFill = order.StopLossOnFill,
+            Type = "MARKET"
+        });
+
+        return await _oandaApiConnection.OrderApi.CreateOrderAsync(account, orderRequest);
+    }
+    
+    
+    public async Task<CreateOrderResponse> OpenShortPosition(string account, double units, double stopLossPrice)
+    {
+        var stoploss = new StopLossDetails(
+            price: stopLossPrice
+        );
+
+        var order = new MarketOrder(
+            InstrumentName.AUD_CHF,
+            units * -1, // short needs to be negative
+            stopLossOnFill: stoploss
+        );
+
+        var orderRequest = new CreateOrderRequest(new
+        {
+            Instrument = order.Instrument,
+            Units = order.Units,
+            StopLossOnFill = order.StopLossOnFill,
+            Type = "MARKET"
+        });
+
+        return await _oandaApiConnection.OrderApi.CreateOrderAsync(account, orderRequest);
     }
 }
