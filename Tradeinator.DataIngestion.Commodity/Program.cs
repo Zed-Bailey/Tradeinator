65b63848e2b7d71c10ab1629cb0c4ccf8a22d6dd
Zoran <emailzed03@gmail.com>
2024-02-29 11:25:53 +1100

    created data ingestion project for commodity data

---
+++ Tradeinator.DataIngestion.Commodity/Program.cs
@@ -0,0 +1,65 @@
+using Coravel;
+using Tradeinator.Configuration;
+using Tradeinator.DataIngestion.Commodity;
+using Tradeinator.DataIngestion.Shared;
+using Serilog;
+using Tradeinator.Shared;
+
+var config = new ConfigurationLoader();
+
+var exchangeHost = config.Get("Rabbit:Host");
+var exchangeName = config.Get("Rabbit:Exchange");
+
+if (exchangeHost is null || exchangeName is null)
+{
+    throw new ArgumentNullException(exchangeHost ?? exchangeName);
+}
+
+// using var exchange = new PublisherExchange(exchangeHost, exchangeName);
+
+// initialise serilog logger, writing to console and file
+await using var logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    // .WriteTo.File("data_ingestion.log")
+    .CreateLogger();
+
+
+
+//
+// var subscriptionManager = new SimpleSubscriptionManager(logger, Directory.GetCurrentDirectory(), "symbols.txt");
+
+var apiToken = config.Get("OANDA_API_TOKEN") ?? throw new ArgumentException("Oanda api token was null or empty");
+var oandaConnection = new OandaConnection(apiToken);
+
+var bar = await oandaConnection.GetLatestData("XAU_AUD");
+logger.Information("Received bar for {Symbol} | {Date} | {O} {H} {L} {C}", 
+    "Gold/AUD", bar.TimeUtc, bar.Open, bar.High, bar.Low, bar.Close
+);
+
+//
+// var builder = Host.CreateApplicationBuilder(args);
+//
+// // add services for worker
+// builder.Services.AddSingleton(subscriptionManager);
+// builder.Services.AddSingleton(exchange);
+// builder.Services.AddSingleton(logger);
+// builder.Services.AddSingleton(oandaConnection);
+//
+// builder.Services.AddTransient<PullDataInvocable>();
+//
+// builder.Services.AddScheduler();
+//
+// var host = builder.Build();
+//
+// host.Services.UseScheduler(scheduler =>
+// {
+//     scheduler
+//         .Schedule<PullDataInvocable>()
+//         .EveryThirtyMinutes();
+//
+// });
+//
+// host.Run();
+//
+// logger.Information("Shutting down");
+
---
+++ Tradeinator.DataIngestion.Commodity/PullDataInvocable.cs
@@ -0,0 +1,66 @@
+using Coravel.Invocable;
+using Serilog.Core;
+using Tradeinator.DataIngestion.Shared;
+using Tradeinator.Shared;
+
+namespace Tradeinator.DataIngestion.Commodity;
+
+public class PullDataInvocable : IInvocable
+{
+    private SimpleSubscriptionManager _subscriptionManager;
+    private PublisherExchange _exchange;
+    private Logger _logger;
+    private OandaConnection _oandaConnection;
+    
+    public PullDataInvocable(SimpleSubscriptionManager subscriptionManager, PublisherExchange exchange, Logger logger, OandaConnection oandaConnection)
+    {
+        _subscriptionManager = subscriptionManager;
+        _exchange = exchange;
+        _logger = logger;
+        _oandaConnection = oandaConnection;
+    }
+
+    /// <summary>
+    /// Checks whether the market is open or not
+    /// </summary>
+    /// <returns>true when market is open, false otherwise</returns>
+    private bool CheckTime()
+    {
+        var time = DateTime.UtcNow;
+        // market is not open on saturday
+        if (time.DayOfWeek is DayOfWeek.Saturday) return false;
+            
+        // market closes friday at 5pm ET or 10pm UTC
+        if (time.DayOfWeek is DayOfWeek.Friday && time.Hour >= 22) return false;
+            
+        // market opens on sunday at 5pm ET or 10pm UTC
+        if(time.DayOfWeek is DayOfWeek.Sunday && time.Hour >= 22) return true;
+            
+            
+        return true;
+    }
+    
+    
+    public async Task Invoke()
+    {
+        if (!CheckTime())
+            return;
+        
+        foreach (var symbol in _subscriptionManager.Symbols)
+        { 
+            var bar = await _oandaConnection.GetLatestData(symbol);
+            if (bar is null)
+            {
+                _logger.Warning("Received bar for {Symbol} was null", symbol);
+                continue;
+            }
+        
+            // post bar to exchange
+            _exchange.Publish(bar, $"bar.{symbol}");
+            _logger.Information("Received bar for {Symbol} | {Date} | {O} {H} {L} {C}", 
+                symbol, bar.TimeUtc, bar.Open, bar.High, bar.Low, bar.Close
+            );
+        
+        }
+    }
+}
